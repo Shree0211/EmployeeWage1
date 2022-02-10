07372c730e2aea4c34626c893e404eb65ffbfc09
shreevyas <shreevyas0211@gmail.com>
2022-02-10 16:55:42 +0530

    Added UC3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("-------------Employee Wage Computation-------------");
 //EmployeeWage1.UseCases.GetAttendence();
-EmployeeWage1.CalculateDailyWages.EmpWage();
-
+//EmployeeWage1.CalculateDailyWages.EmpWage();
+EmployeeWage1.EmployeePartTime.PartTimeEmpWage();
